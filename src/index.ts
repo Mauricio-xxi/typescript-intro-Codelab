bc33af53f78230bca8edfd62aeb93140e8d109ba
Álvaro Camacho <37156417+Mauricio-xxi@users.noreply.github.com>
2024-03-17 19:06:22 +0100

    refactor: clean the index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-import { DemoComponent } from "@components/demo.components";
+import { DemoComponent } from '@components/demo.component';
 
-const a: string = "Hello, World!";
-const b: number = 1;
-const c: boolean = true;
-const d: null = null;
-const e: undefined = undefined;
-const f: symbol = Symbol("foo");
-// const g: bigint = 100n; // esto nos dará un error porque tenemos que configurar el target de TypeScript a ES2020. Lo veremos en el siguiente apartado.
-const h: any = "Hello, World!";
-const i: unknown = "Hello, World!";
-const j: string[] = ["Hello", "World"];
-const k: Array<string> = ["Hello", "World"];
-// definimos una función que recibe un number y devuelve un boolean
-const l: (param: number) => boolean = (param: number) => param > 0;
+console.log(DemoComponent.template);
